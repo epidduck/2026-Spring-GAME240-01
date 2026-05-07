dea81bcc860a7ced458d57deee3138acd414d6bc
epidduck <pidduck@chapman.edu>
2026-05-06 20:57:16 -0700

    update

--- homework/april_27/Fishmonger Files/Fishmonger Files/Program.cs
+++ homework/april_27/Fishmonger Files/Fishmonger Files/Program.cs
@@ -59,7 +59,6 @@ while (runLogLoop == "true")
 Console.WriteLine("Where do you want to save the result?");
 string resultPath = Console.ReadLine();
 
-//creating results
 //declaring special fish
 string specialFish;
 specialFish = "";
@@ -91,7 +90,40 @@ else
 }
 
 //counting up total fish
+        //splitting lines
+string[] lines = log.Split(new string[] {Environment.NewLine}, StringSplitOptions.None);
+
+        //splitting words
+int lineNumber;
+lineNumber = 0;
+int sum = 0;
+int specialSum = 0;
+while (lineNumber < lines.Length)
+{
+    string[] words;
+    words = lines[lineNumber].Split(' ');
+    Console.WriteLine(words[0]);
+    sum = sum + int.Parse(words[0]);
+    if (lines[lineNumber].Contains(specialFish))
+    {
+        specialSum = specialSum + int.Parse(words[0]);
+    }
+    lineNumber++;
+}
+
+//writing results
+
+Console.WriteLine("Today's special is " + specialFish);
+Console.WriteLine("Total " + specialFish + " caught: " + specialSum);
+Console.WriteLine("Total fish caught: " + sum);
 
 
 
-
+/*
+using (StreamWriter resultFile = new StreamWriter(resultPath))
+{
+    resultFile.WriteLine("Today's special is " + specialFish);
+    resultFile.WriteLine("Total " + specialFish + " caught: " + specialSum);
+    resultFile.WriteLine("Total fish caught: " + sum);
+}
+*/
